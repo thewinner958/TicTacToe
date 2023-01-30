bb456db720877580f4d14d2a76a0e945b2e2e998
TheWinner <thewinner958@gmail.com>
2023-01-30 17:02:44 +0200

    created a Player class

---
+++ src/main/java/github/thewinner958/tictactoe/game/Player.java
@@ -0,0 +1,52 @@
+package github.thewinner958.tictactoe.game;
+
+import java.util.Objects;
+
+public class Player {
+    private final String username;
+    private final char symbol;
+    private final int wins;
+    private final int loses;
+
+    public Player() {
+        username = "Computer";
+        symbol = 'O';
+        wins = 0;
+        loses = 0;
+    }
+
+    public Player(String username, char symbol, int wins, int loses) {
+        this.username = username;
+        this.symbol = symbol;
+        this.wins = wins;
+        this.loses = loses;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLoses() {
+        return loses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player player)) return false;
+        return Objects.equals(getUsername(), player.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername());
+    }
+}
