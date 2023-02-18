400a629ce51ae8ad45d66775409942f6df785e77
TheWinner <thewinner958@gmail.com>
2023-02-18 22:33:52 +0200

    added the PlayerController

---
+++ src/main/java/github/thewinner958/tictactoe/web/PlayerController.java
@@ -0,0 +1,30 @@
+package github.thewinner958.tictactoe.web;
+
+import github.thewinner958.tictactoe.data.entities.Player;
+import github.thewinner958.tictactoe.game.services.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("players")
+public class PlayerController {
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping(path = "/all")
+    public List<Player> getAllPlayers() {
+        return playerService.listPlayers();
+    }
+
+    @GetMapping
+    public Player getPlayerById(@RequestParam int id) {
+        return playerService.getPlayerById(id);
+    }
+
+}
