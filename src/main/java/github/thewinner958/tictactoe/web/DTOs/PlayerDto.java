1736326f8a92ff38eb164a9a69799e2eec369ef0
TheWinner <thewinner958@gmail.com>
2023-02-23 13:28:52 +0200

    made the dto so that it doesn't send the password in json body

--- src/main/java/github/thewinner958/tictactoe/web/DTOs/PlayerDto.java
+++ src/main/java/github/thewinner958/tictactoe/web/DTOs/PlayerDto.java
@@ -1,11 +1,13 @@
 package github.thewinner958.tictactoe.web.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.io.Serializable;
 import java.time.Instant;
 
 /**
  * A DTO for the {@link github.thewinner958.tictactoe.data.entities.Player} entity
  */
-public record PlayerDto(int id, String username, String email, String password, Instant createTime, byte isBot) implements Serializable {
+public record PlayerDto(Integer id, String username, String email, @JsonIgnore String password, Instant createTime, @JsonIgnore Byte isBot) implements Serializable {
 
 }
