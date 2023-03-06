c99e5d4aa038bf813300f147206650e59e97a79d
TheWinner <thewinner958@gmail.com>
2023-03-06 19:21:55 +0200

    added some functionality to the GameRepository

--- src/main/java/github/thewinner958/tictactoe/data/repositories/GameRepository.java
+++ src/main/java/github/thewinner958/tictactoe/data/repositories/GameRepository.java
@@ -1,8 +1,19 @@
 package github.thewinner958.tictactoe.data.repositories;
 
 import github.thewinner958.tictactoe.data.entities.Game;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import github.thewinner958.tictactoe.data.entities.Player;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
+    @Transactional
+    @Modifying
+    @Query("update Game g set g.gameStatus = ?1, g.whoWon = ?2 where g.id = ?3")
+    int updateGameStatusAndWhoWonById(Boolean gameStatus, Boolean whoWon, Integer id);
+    @Transactional
+    @Modifying
+    @Query("update Game g set g.player2 = ?1 where g.id = ?2")
+    int updatePlayer2ByGameId(Player player2, Integer id);
 }
