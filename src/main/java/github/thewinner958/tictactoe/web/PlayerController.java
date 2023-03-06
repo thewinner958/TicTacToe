b3ef152d30b58547b5f3206674a8609dbabee9f8
TheWinner <thewinner958@gmail.com>
2023-03-06 19:14:37 +0200

    optimized import

--- src/main/java/github/thewinner958/tictactoe/web/PlayerController.java
+++ src/main/java/github/thewinner958/tictactoe/web/PlayerController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("players")
