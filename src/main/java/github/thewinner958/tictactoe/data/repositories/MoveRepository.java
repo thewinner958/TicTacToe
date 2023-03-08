6d0537a630f21f357b498e812ca93565d74d025f
TheWinner <thewinner958@gmail.com>
2023-03-08 23:38:25 +0200

    removed unnecessary function

--- src/main/java/github/thewinner958/tictactoe/data/repositories/MoveRepository.java
+++ src/main/java/github/thewinner958/tictactoe/data/repositories/MoveRepository.java
@@ -1,7 +1,6 @@
 package github.thewinner958.tictactoe.data.repositories;
 
 import github.thewinner958.tictactoe.data.entities.Move;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,5 +9,4 @@ import java.util.List;
 public interface MoveRepository extends CrudRepository<Move, Integer>{
     @Query("select m from Move m where m.game.id = ?1")
     List<Move> getAllMovesByGameId(Integer id);
-    List<Move> findByGame_Id(Integer id);
 }
